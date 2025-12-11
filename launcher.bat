fb52b366aa1edba187c79795085e642da2e97dd1
Eduardo Gladzik <eduardogladzik@gmail.com>
2025-12-11 11:17:44 -0300

    The launcher logic has been enhanced to function as a controller, indicating when each script should be executed.

--- launcher.bat
+++ launcher.bat
@@ -1,15 +1,28 @@
 @echo off
+set "WORK_DIR=%~dp0"
+set "TEMP_DIR=%SystemRoot%\Temp\WinConfig"
+set "FLAG_FILE=%SystemRoot%\Temp\WinConfig_Stage2.flag"
 
+:: Verify admin permissions
 >nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
 if '%errorlevel%' NEQ '0' (
     echo Solicitando privilegios de Administrador...
-    powershell.exe -NoProfile -Command "Start-Process -FilePath '%~f0' -Verb RunAs"
+    powershell.exe -Command "Start-Process -FilePath '%~f0' -Verb RunAs"
     exit /b
 )
 
-echo Iniciando a Ferramenta de Configuracao...
-powershell.exe -ExecutionPolicy Bypass -File "%~dp0\script-winconfig.ps1"
+:: =======================================================
+:: Report and Winconfig
+:: =======================================================
+echo.
+echo [PASSO 1] Gerando Relatorios...
+powershell.exe -ExecutionPolicy Bypass -File "%WORK_DIR%scripts\report.ps1"
 
 echo.
-echo Processo terminado. Pressione qualquer tecla para fechar...
-pause > nul
+echo [PASSO 2] Iniciando GUI de Configuracao...
+start /wait powershell.exe -ExecutionPolicy Bypass -File "%WORK_DIR%scripts\winconfig.ps1"
+
+echo.
+echo Processo finalizado com sucesso!
+pause
+exit
