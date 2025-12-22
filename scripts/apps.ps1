83c818e101be2206facd1c5eb164a032c014890e
Eduardo Gladzik <eduardogladzik@gmail.com>
2025-12-22 16:00:58 -0300

    chocolatey installation logic replaced with winget installation logic.

--- scripts/apps.ps1
+++ scripts/apps.ps1
@@ -1,41 +1,26 @@
-function Install-ChocoPrograms {
-    Write-Host "Iniciando Gerenciador de Pacotes Chocolatey..."
+function Execute-InstallationFile {
+    Write-Host "Iniciando Gerenciador de Pacotes do WIndows..."
 
-    # LOCATE PACKAGES.CONGI
+    # LOCATE pckgs.JSON
     $scriptDir = $PSScriptRoot
 	$rootFolder = Split-Path -Parent $scriptDir
-    $configFile = Join-Path $rootFolder "chocolatey\packages.config"
+    $configFile = Join-Path $rootFolder "winget\pckgs.JSON"
 
     if (-not (Test-Path $configFile)) {
-        throw "Arquivo 'packages.config' não encontrado na pasta: $scriptPath. A instalação será abortada."
+        throw "Arquivo 'pckgs.JSON' não encontrado na pasta: $scriptPath. A instalação será abortada."
     }
     Write-Host "  > Arquivo de configuração encontrado: $configFile"
 
-    # INSTALL CHOCOLATEY
-    if (!(Test-Path "$env:ProgramData\chocolatey\bin\choco.exe")) {
-        Write-Host "  > Chocolatey não encontrado. Instalando..."
-        try {
-            [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
-            Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1')) | Out-Null
-            
-            $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
-        }
-        catch { throw "Falha ao baixar Chocolatey. Verifique o acesso a internet!" }
-    }
-
-    $chocoExe = "$env:ProgramData\chocolatey\bin\choco.exe"
-    if (!(Test-Path $chocoExe)) { throw "Erro Crítico: Executável do Chocolatey não encontrado." }
-
     # EXECUTE INTALATION WITH PACKAGES.CONFIG
-    Write-Host "  > Instalando aplicativos definidos em packages.config..."
+    Write-Host "  > Instalando aplicativos definidos no arquivo de intalação..."
     
-    & $chocoExe install $configFile -y --limit-output
+    & winget import -i $configFile
     
     if ($LASTEXITCODE -eq 0) {
         Write-Host "  > Instalação de pacotes concluída!"
     } else {
-        Write-Warning "  > Chocolatey finalizou com avisos."
+        Write-Warning "  > Winget finalizou com avisos."
     }
 }
 
-Install-ChocoPrograms
+Execute-InstallationFile
