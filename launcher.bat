56323b2c2b5ac089aa8775e339ed56669712402c
Eduardo Gladzik <eduardogladzik@gmail.com>
2025-12-23 20:58:08 -0300

    The launcher now executes only the GUI.

--- launcher.bat
+++ launcher.bat
@@ -1,32 +1,17 @@
 @echo off
 set "WORK_DIR=%~dp0"
 set "TEMP_DIR=%SystemRoot%\Temp\WinConfig"
-set "FLAG_FILE=%SystemRoot%\Temp\WinConfig_Stage2.flag"
 
 :: Verify admin permissions
 >nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
 if '%errorlevel%' NEQ '0' (
-    echo Solicitando privilegios de Administrador...
+    echo Solicitando privilegios de administrador...
     powershell.exe -Command "Start-Process -FilePath '%~f0' -Verb RunAs"
     exit /b
 )
 
-:: =======================================================
-:: Report and Winconfig
-:: =======================================================
-echo.
-echo [PASSO 1] Gerando Relatorios...
-powershell.exe -ExecutionPolicy Bypass -File "%WORK_DIR%scripts\report.ps1"
+:: Chama a GUI
+echo Iniciando Interface...
+powershell.exe -ExecutionPolicy Bypass -File "%WORK_DIR%scripts\interface.ps1"
 
-echo.
-echo [PASSO 2] Iniciando Instalação dos aplicativos padrão...
-start /wait powershell.exe -ExecutionPolicy Bypass -File "%WORK_DIR%scripts\apps.ps1"
-
-echo.
-echo [PASSO 3] Iniciando GUI de Configuracao...
-start /wait powershell.exe -ExecutionPolicy Bypass -File "%WORK_DIR%scripts\winconfig.ps1"
-
-echo.
-echo Processo finalizado com sucesso!
-pause
 exit
